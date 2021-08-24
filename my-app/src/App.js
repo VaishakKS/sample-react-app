5c7070f065d74bf10dc372685d2d2f6a2f06d37a
VaishakKS <vaishakmnglr@gmail.com>
2021-08-24 07:09:24 +0000

    state example commit

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import SearchResult from './Main';
+import { Counter } from "./Counter";
 
-function App() {
+function App(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Understanding State</h1>
+      <Counter></Counter>
+      <Counter></Counter>
     </div>
   );
 }
-
 export default App;
---
+++ my-app/src/Counter.js
@@ -0,0 +1,41 @@
+import React from "react";
+import './Counter.css';
+
+export class Counter extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { count: 0, isStarted: false };
+        this.toggleButton = this.toggleButton.bind(this);
+    }
+
+    toggleButton() {
+        if (!this.state.isStarted) {
+           // clicked Start button, so start the timer
+          this.counterInterval = setInterval(() => {
+            // Update the counter state
+            this.setState((state) => ({
+              count: state.count + 1,
+            }));
+          }, 1000);
+        } else {
+           // clicked stopped button, so clear the timer
+          clearInterval(this.counterInterval);
+        }
+        // update the isStarted state
+        this.setState({
+          isStarted: !this.state.isStarted,
+        });
+    }
+
+    render() {
+        return (
+          <div className="counter">
+            <button className="btn" onClick={this.toggleButton}>
+              {this.state.isStarted ? "Stop" : "Start"}
+            </button>
+    
+            <span>Count is {this.state.count}</span>
+          </div>
+        );
+    }
+}
